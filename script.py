a29947463349d8710ca1bc3e5aec88e2c60a825f
le-goat <58209708+le-goat@users.noreply.github.com>
2020-03-09 14:08:09 +0100

    Ajout de la fonction exemple

--- script.py
+++ script.py
@@ -2,5 +2,6 @@ print("Hello world")
 print("deuxième fonction")
 compteur = 0
 print("compteur vaut" + str(compteur))
+print("fonction exemple")
 # début de la branche develop
 print("fin du programme")
