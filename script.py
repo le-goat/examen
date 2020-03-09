d3b90e3b9393639bd875dc2cf5dd063d62595739
le-goat <58209708+le-goat@users.noreply.github.com>
2020-03-09 14:08:41 +0100

    Ajout de la fonction somme

--- script.py
+++ script.py
@@ -3,5 +3,6 @@ print("deuxième fonction")
 compteur = 0
 print("compteur vaut" + str(compteur))
 print("fonction exemple")
+print("fonction somme")
 # début de la branche develop
 print("fin du programme")
